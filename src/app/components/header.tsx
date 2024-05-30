c241a630fc7fc121b04259d91de14c1eb25a9017
Andrew Magill <AndyMagill@Gmail.com>
2024-05-30 17:15:13 -0400

    added css files

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faSave } from '@fortawesome/free-solid-svg-icons';
+import '../styles/components/header.css';
 
 export default function Header() {
 
@@ -12,10 +12,10 @@ export default function Header() {
         ColorScheme.IO
       </div>
 
-      <nav className="nav-icons">
-        <ul>
-          <li><a href="/" className="mx-2"><FontAwesomeIcon icon={faHome} /><span>Home</span></a></li>
-          <li><a href="/saved" className="mx-2"><FontAwesomeIcon icon={faSave} /><span>Saved Schemes</span></a></li>
+      <nav>
+        <ul className="flex gap-1">
+          <li><a href="/" className="p-1"><FontAwesomeIcon icon={faHome} /><span>Home</span></a></li>
+          <li><a href="/saved" className="p-1"><FontAwesomeIcon icon={faSave} /><span>Saved Schemes</span></a></li>
         </ul>
       </nav>
     </header>
