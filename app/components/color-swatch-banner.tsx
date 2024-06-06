ac0cbbf8fe312b2458337d488877582064259d12
Andrew Magill <AndyMagill@Gmail.com>
2024-06-06 06:47:22 -0400

    added debouce util to colorswatchbanner

--- app/components/color-swatch-banner.tsx
+++ app/components/color-swatch-banner.tsx
@@ -1,45 +1,54 @@
-"use client"
+"use client";
+
 import React, { useState, useEffect } from 'react';
+import { debounce } from '../utils/debounce';
 
 const ColorSwatchBanner = () => {
   const [swatchCount, setSwatchCount] = useState(0);
 
   useEffect(() => {
-    // Calculate the number of swatches based on the window width
-    const updateSwatchCount = () => {
+    // Debounce the updateSwatchCount function
+    const debouncedUpdateSwatchCount = debounce(() => {
       const swatchSize = window.innerHeight * 0.1; // 10vh
       const newSwatchCount = Math.floor(window.innerWidth / swatchSize);
       setSwatchCount(newSwatchCount);
-    };
+    }, 100); // 0.1 seconds
 
     // Set the initial swatch count on mount
-    updateSwatchCount();
+    debouncedUpdateSwatchCount();
 
     // Add event listener for window resize
-    window.addEventListener('resize', updateSwatchCount);
+    window.addEventListener('resize', debouncedUpdateSwatchCount);
 
     // Clean up event listener
-    return () => window.removeEventListener('resize', updateSwatchCount);
+    return () => window.removeEventListener('resize', debouncedUpdateSwatchCount);
   }, []);
 
   // Generate an array of swatches with incremental hues
   const hueIncrement = 360 / swatchCount;
   const swatches = Array.from({ length: swatchCount }, (_, index) => {
     const hue = index * hueIncrement;
-    const invert = ( hue + 180 > 360 ) ? hue - 180 : hue + 180;
+    const invert = (hue + 180) % 360;
 
     return (
       <div
         key={index}
-        data-hover-outline-color={invert}
-        className="inline-block w-[10vh] h-[10vh] outline-4 outline-transparent hover:outline-[hsl(attr('data-hover-outline-color), 98%, 48%)]"
-        style={{ backgroundColor: `hsl(${hue}, 98%, 48%)` }}
+        className="inline-block w-[10vh] h-[10vh] outline-4 outline-transparent"
+        style={{
+          backgroundColor: `hsl(${hue}, 98%, 48%)`,
+        }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.outlineColor = `hsl(${invert}, 98%, 48%)`;
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.outlineColor = 'transparent';
+        }}
       />
     );
   });
 
   return (
-    <div className="flex w-full gap-1 py-1 items-center ">
+    <div className="flex w-full gap-1 py-1 items-center">
       {swatches}
     </div>
   );
